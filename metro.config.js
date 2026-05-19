b1dcf3d2f5c9c8369a4d2ec032f7e46e3d48ae78
Lais <seuemail@gmail.com>
2026-05-19 19:37:14 -0300

    AJUSTES MANUAIS

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,9 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
+
+// Ensure public folder is accessible
+config.watchFolders = [__dirname];
+config.resolver.assetExts = [...config.resolver.assetExts, "json"];
 
 module.exports = config;
